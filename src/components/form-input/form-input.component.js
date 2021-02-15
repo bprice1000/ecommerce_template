9b04712695c4f60f49da1aae696bbf24bfeeca5f
Benjamin Price <price.bj@gmail.com>
2021-02-15 14:18:49 -0600

    added signup page and firebase with signup, fixed error on ternary of form-input preventing label shrink

--- src/components/form-input/form-input.component.js
+++ src/components/form-input/form-input.component.js
@@ -4,10 +4,9 @@ import './form-input.styles.scss';
 const FormInput = ({handleChange, label, ...otherProps}) => (
   <div className='group'>
     <input className='form-input' onChange={handleChange} {...otherProps} />
-    {
-      label ?  //label always has classname of form-input-label, but also has the className shrink when user has typed
+    { label ?
       (<label
-      className={`${otherProps.value.length} ? 'shrink' : ''}
+      className={`${otherProps.value.length ? 'shrink' : ''}
       form-input-label`}>{label}</label>
       ) : null
     }
