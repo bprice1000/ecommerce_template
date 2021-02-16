250ec496ff50862e7b7f1cbf25eb9d053d5ca7a1
Benjamin Price <price.bj@gmail.com>
2021-02-16 06:33:26 -0600

    added cart redux components for hidden toggle

--- src/components/header/header.component.js
+++ src/components/header/header.component.js
@@ -7,7 +7,7 @@ import CartIcon from '../cart-icon/cart-icon.component'
 import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link to='/' className='logo-container'>
       <Logo className='logo' />
@@ -33,13 +33,17 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
+    {
+      hidden ? null :
     <CartDropdown />
+    }
   </div>
 
 )
 
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+  currentUser,
+  hidden
 })
 
 export default connect(mapStateToProps)(Header);
