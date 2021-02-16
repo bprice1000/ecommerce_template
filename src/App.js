c5c3ea1085b139d9f529f3348f22f8da8d9bc302
Benjamin Price <price.bj@gmail.com>
2021-02-15 20:10:10 -0600

    add redirect for signed in

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setCurrentUser } from './redux/user/user.actions'
 import './App.css';
@@ -42,8 +42,10 @@ class App extends React.Component {
     <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/shop' component={ShopPage} />
-        <Route exact path='/signin' component={SignInAndSignUpPage} />
+        <Route path='/shop' component={ShopPage} />
+        <Route exact path='/signin' render={() => this.props.currentUser ?
+        (<Redirect to='/' />) :
+        (<SignInAndSignUpPage />)} />
       </Switch>
 
     </div>
@@ -51,9 +53,13 @@ class App extends React.Component {
   }
 }
 
+const mapStateToProps = ({ user}) => ({
+  currentUser: user.currentUser
+})
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 
 })
 
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
